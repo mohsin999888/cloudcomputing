31b9be6fd34d1b63f3d123f0ca2c2cb584a0b4fd
mohsin999888 <71980631+mohsin999888@users.noreply.github.com>
2020-09-27 21:52:43 +0500

    Update and rename sccript.sh to script.sh

--- script.sh
+++ script.sh
@@ -2,28 +2,14 @@ sudo apt-get update
 
 sudo apt install apache2 --assume-yes
 
-http://192.168.72.179
-
-sudo ufw show app list
-
 sudo ufw allow 'Apache'
-
-sudo ufw status
 
 sudo systemctl start apache2.service
 
 wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.11-1_all.deb
 
-sudo dpkg -i mysql-apt-config_0.8.11-1_all.deb
-
 sudo apt-get update
 
 sudo apt install mysql-server --assume-yes
 
-sudo mysql_secure_installation
-
-sudo service mysql status
-
 sudo service mysql start
-
-sudo mysql -u root -p
